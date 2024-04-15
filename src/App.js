2ec59a522ca9bb113b7770d6f10951b3b611c12c
Linus Xu <linus_xu@live.com>
2024-04-15 14:09:32 -0400

    Simple mechanism to support different JSON structures.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+import apiList from './components/constants';
 
 const App = () => {
   const [pic, setPic] = useState(<img src={logo} className="App-logo" alt="logo" />);
   const [isLoading, setIsLoading] = useState(false);
+  const [api, setApi] = useState('');
   const getPic = (site, name = site) => {
     setIsLoading(true);
     fetch(site)
       .then((res) => res.json())
-      .then((json) => json.image)
+      .then((json) => json[apiList[api]])
       .then((img) => setPic(<img src={img} alt={`Random meme from ${name}`} />))
       .then(() => setIsLoading(false));
   }
   return (
     <div className="App App-header">
       {isLoading ? "Loading..." : pic}
-      <button onClick={() => getPic('https://yesno.wtf/api')}>Get random <s>xkcd</s> comic</button>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          {api === '' ? 'Site List' : api}
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          {Object.keys(apiList).map((api, index) => <Dropdown.Item key={index} onClick={() => setApi(api)}>{api}</Dropdown.Item>)}
+        </Dropdown.Menu>
+      </Dropdown>
+      <button disabled={api === ''} onClick={() => getPic(api)}>{api === '' ? 'Select a site to get started' : `Get random comic from ${api}`}</button>
       <p>
         SI579 Final Project: Meme search engine
       </p>
---
+++ src/components/constants.js
@@ -0,0 +1,6 @@
+const apiList = {
+    'https://yesno.wtf/api': 'image',
+    'https://dog.ceo/api/breeds/image/random': 'message'
+};
+
+export default apiList;
