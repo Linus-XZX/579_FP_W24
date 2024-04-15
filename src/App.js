4c96fbb1478bc594c30b5ca29cd53fd10cad7b28
Linus Xu <linus_xu@live.com>
2024-04-15 13:34:03 -0400

    A somewhat working version with pic refreshing

--- src/App.js
+++ src/App.js
@@ -3,30 +3,31 @@ import './App.css';
 import {useState} from "react";
 
 const App = () => {
-  const [pic, setPic] = useState(logo);
-  const getPic = () => {
-    fetch('https://xkcd.com/info.0.json')
+  const [pic, setPic] = useState(<img src={logo} className="App-logo" alt="logo" />);
+  const [isLoading, setIsLoading] = useState(false);
+  const getPic = (site, name = site) => {
+    setIsLoading(true);
+    fetch(site)
       .then((res) => res.json())
-      .then((json) => json.img)
-      .then((img) => setPic(img));
+      .then((json) => json.image)
+      .then((img) => setPic(<img src={img} alt={`Random meme from ${name}`} />))
+      .then(() => setIsLoading(false));
   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={pic} className="App-logo" alt="logo" />
-        <button onClick={getPic}>Get random xkcd comic</button>
-        <p>
-          SI579 Final Project: Meme search engine
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/Linus-XZX/579_fp_w24"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Link to source code repo
-        </a>
-      </header>
+    <div className="App App-header">
+      {isLoading ? "Loading..." : pic}
+      <button onClick={() => getPic('https://yesno.wtf/api')}>Get random <s>xkcd</s> comic</button>
+      <p>
+        SI579 Final Project: Meme search engine
+      </p>
+      <a
+        className="App-link"
+        href="https://github.com/Linus-XZX/579_fp_w24"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Link to source code repo
+      </a>
     </div>
   );
 }
